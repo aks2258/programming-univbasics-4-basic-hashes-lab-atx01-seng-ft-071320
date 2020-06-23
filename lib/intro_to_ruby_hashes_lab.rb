9926629a1ae1d86eaba02ba87ceae7eb6da43a1c
Arnie Serrano <arniekidd@gmail.com>
2020-06-23 20:25:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -14,10 +14,14 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   {
-    :name
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  {
+    id:
+  }
 end
